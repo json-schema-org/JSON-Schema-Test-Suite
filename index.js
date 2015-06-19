b508ede5c999f7616d13095a29494072ae7bd957
Tony Pujals <tonypujals@users.noreply.github.com>
2015-06-18 17:15:38 -0700

    refactored filter

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 var fs = require('fs'),
     path = require('path');
 
-exports.loadSync = loadTestSuiteSync;
+module.exports = {
+  loadSync: loadTestSuiteSync,
+
+  requiredOnlyFilter: function(file, parent, optional) {
+    return !optional;
+  },
+
+  optionalOnlyFilter: function(file, parent, optional) {
+    return optional;
+  }
+}
 
 /**
  * Returns tests specified by draft and filtered by filter argument
  * @param draft 'draft3' | 'draft4' (default)
- * @param filter 'required' | 'optional' | 'all' (default)
+ * @param filter a function that returns true if the file (or directory) should be included; see exported requiredOnlyFilter
  * @returns []
  */
 function loadTestSuiteSync(draft, filter) {
-  return loadTestsSync([], path.join(__dirname, 'tests', draft || 'draft4'));
+  return loadTestsSync([], path.join(__dirname, 'tests', draft || 'draft4'), filter);
 }
 
 /**
@@ -21,22 +31,21 @@ function loadTestSuiteSync(draft, filter) {
  * @param markOptional set to true when recursing into optional directory
  * @returns []
  */
-function loadTestsSync(tests, draftPath, filter, markOptional) {
-  filter = filter || 'all';
+function loadTestsSync(tests, draftPath, filter, parent, markOptional) {
   markOptional = !!markOptional;
 
   var testFiles = fs.readdirSync(draftPath);
 
   testFiles.filter(function (file) {
-    return ((filter == 'all')
-      || (filter != 'required' && (markOptional || file == 'optional'))
-      || (filter != 'optional' && !markOptional));
+    // markOptional might not be set at this point, so still need to check if the file is named 'optional'
+    return filter ? filter(file, parent, markOptional || file == 'optional') : true;
   }).forEach(function (file) {
     var filePath = path.join(draftPath, file);
     var stats = fs.lstatSync(filePath);
 
-    if (stats.isDirectory() && file == 'optional') {
-      loadTestsSync(tests, path.join(draftPath, file), filter, true);
+    if (stats.isDirectory()) {
+      // once markOptional is set after entering an 'optional' directory, no need to reevaluate
+      loadTestsSync(tests, path.join(draftPath, file), filter, file, markOptional || file == 'optional');
     } else if (stats.isFile()) {
       var end = file.indexOf('.json');
       if (end > 0) {
--- test/test.js
+++ test/test.js
@@ -1,6 +1,4 @@
 var assert = require('assert'),
-    fs = require('fs'),
-    path = require('path'),
     glob = require('glob'),
     util = require('util'),
     suite = require('..');
@@ -8,32 +6,45 @@ var assert = require('assert'),
 describe('verify test suite loads all json test files', function () {
 
   var testMessage = 'The number of %s test groups should match the number of %s json files';
-  var errorMessage = 'expected the number of test groups to match the number of %s json files';
+  var errorMessage = 'the actual number of test groups was expected match the number of %s json files';
 
   var drafts = [ 'draft3', 'draft4' ];
 
   var allPattern = './tests/%s/**/*.json';
-  var requiredPattern = './tests/%s/**/*.json';
-  var optionalPattern = './tests/%s/**/*.json';
+  var requiredPattern = { glob: './tests/%s/**/*.json', ignore: './tests/%s/optional/*.json' };
+  var optionalPattern = './tests/%s/optional/*.json';
+  var minPattern = './tests/%s/**/min*.json';
 
   var patternTests = [
-    { pattern: allPattern, filter: 'ALL' },
-    { pattern: requiredPattern, filter: 'REQUIRED' },
-    { pattern: optionalPattern, filter: 'OPTIONAL' }
+    { name: 'all', globPattern: allPattern },
+    { name: 'required', globPattern: requiredPattern, filter: suite.requiredOnlyFilter },
+    { name: 'optional', globPattern: optionalPattern, filter: suite.optionalOnlyFilter },
+    { name: '"min"-prefixed', globPattern: minPattern, filter: function(file) {
+      return /^min/.test(file);
+    }}
   ];
 
-  function compareCount(draft, pattern, filter) {
-    it (util.format(testMessage, draft, filter), function() {
-      var tests = suite.loadSync(draft);
-      var files = glob.sync(util.format(pattern, draft));
+  function compareCount(draft, globPattern, filter, name) {
+    it (util.format(testMessage, draft, name), function() {
+      var tests = suite.loadSync(draft, filter);
+      var files;
 
-      assert.equal(tests.length, files.length, util.format(errorMessage, filter));
+      if (typeof globPattern == 'string') {
+        files = glob.sync(util.format(globPattern, draft));
+      } else {
+        files = glob.sync(util.format(globPattern.glob, draft), {
+          ignore: util.format(globPattern.ignore, draft)
+        });
+      }
+
+      console.log('actual (suite): %d, expected (files): %d', tests.length, files.length);
+      assert.equal(tests.length, files.length, util.format(errorMessage, name));
     });
   }
 
   drafts.forEach(function(draft) {
     patternTests.forEach(function(pt) {
-      compareCount(draft, pt.pattern, pt.filter);
+      compareCount(draft, pt.globPattern, pt.filter, pt.name);
     })
   });
 });
