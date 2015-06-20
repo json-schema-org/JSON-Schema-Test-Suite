335c132bf5fd92b46a6e2831a7ecbabf3a343c98
Tony Pujals <tonypujals@users.noreply.github.com>
2015-06-19 22:33:20 -0700

    refactoring, updated docs and README.md

--- README.md
+++ README.md
@@ -9,32 +9,162 @@ fork makes the test suite available as an npm package for use with Node.js.
 
 ### Usage:
 
-    var draft4 = require('@atomiq/json-schema-test-suite').draft4;
-    var validator = require('JSON-SCHEMA-VALIDATOR'); // such as tv4 or z-schema
+There are a number of ways of loading the tests:
 
-    // enumerate ALL tests
-    draft4.all.forEach(function(test) {
-      ...
-    });
+    var testSuite = require('@atomiq/json-schema-test-suite');
 
-    // enumerate REQUIRED tests
-    draft4.required.forEach(function(test) {
-      ...
-    });
+    // this will load all (required and optional) draft4 tests
+    var tests = testSuite.loadSync();
 
-    // enumerate OPTIONAL tests
-    draft4.optional.forEach(function(test) {
-      ...
-    });
+    // optional `filter` is a function that takes 3 arguments (filename, parent, optional)
+    // and returns true if the test should be included. The optional argument is true
+    // for all files under the `<draft>/optional` directory.
+    // optional `draft` should be either `'draft3'` or `'draft4'`
+    var tests = testSuite.loadSync(filter, draft);
+
+    // convenience functions:
+
+    // The following take an optional `filter` as described previously (undefined will load all tests)
+    var draft3 = testSuite.draft3();
+    var draft4 = testSuite.draft4();
+
+    // The following take an optional `draft` argument (defaults to 'draft4')
+    var all = testSuite.loadAllSync();
+    var required = testSuite.loadRequiredSync();
+    var optional = testSuite.loadOptionalSync();
+
+    
+The return value of these functions is an array of objects that correspond to each file under `tests/<draft>` that
+passed the filter (the default is all, so the array will also include all the optional files).
+
+Each object has the following structure (using `tests/draft4/additionalItems.json` as an example):
+
+```
+{
+  group:    'additionalItems',
+  file:     'additionalItems.json',
+  optional: false,  // true if a file under the optional directory
+  path:     '/full/path/to/JSON-Schema-Test-Suite/tests/draft4/additionalItems.json',
+  schemas:  []
+}
+```
+
+The `schemas` property contains the array of objects loaded from the test file.
+Each object consists of a schema and description, along with a number of tests used for validation. Using the first schema object in the array from `tests/draft4/additionalItems.json` as an example:
+
+```
+{
+  description: 'additionalItems as schema',
+  schema: {
+    items: [{}],
+    additionalItems: { type: "integer" }
+  },
+  tests: [
+    {
+      description: "additional items match schema",
+      data: [ null, 2, 3, 4 ],
+      valid: true
+    },
+    {
+      description: "additional items do not match schema",
+      data: [ null, 2, 3, "foo" ],
+      valid: false
+    }
+  ]
+}
+```
+
+### Testing a JSON Validator
+
+You can apply a validator against all the tests. You need to create a validator factory function that takes a JSON schema and an options argument, and returns an object with a validate method. The validate function should take a JSON object to be validated against the schema. It should return an object with a valid property set to true or false, and if not valid, an errors property that is an array of one or more validation errors.
+
+The following are examples of `Tiny Validator (tv4)` and `z-schema` validator factories used by the unit test.
 
 
-### Package Tests
+#### tv4
+```
+var tv4 = require('tv4');
 
-If you clone the package, you can validate the test suite using a factory for either z-schema or tv4 (default).
+var tv4Factory = function (schema, options) {
+  return {
+    validate: function (json) {
+      try {
+        var valid = tv4.validate(json, schema);
+        return valid ? { valid: true } : { valid: false, errors: [ tv4.error ] };
+      } catch (err) {
+        return { valid: false, errors: [err.message] };
+      }
+    }
+  };
+};
+```
 
-    FACTORY=<tv4|z-schema> npm test
+#### ZSchema
 
-For the remote ref tests, a server will be automatically started that listens to port 1234 and serves files from the `remotes` directory.
+```
+var ZSchema = require('z-schema');
+
+var zschemaFactory = function (schema, options) {
+  var zschema = new ZSchema(options);
+
+  return {
+    validate: function (json) {
+      try {
+        var valid = zschema.validate(json, schema);
+        return valid ? { valid: true } : { valid: false, errors: zschema.getLastErrors() };
+      } catch (err) {
+        return { valid: false, errors: [err.message] };
+      }
+    }
+  };
+};
+```
+
+#### Testing the Validator
+
+Using a validator factory as described above, you can test it as follows.
+
+```
+var testSuite = require('@atomiq/json-schema-test-suite');
+var factory = require('YOUR-FACTORY');
+
+var options = { ... };
+
+var tests = testSuite.testSync(factory, options);
+```
+
+The `tests` return value is as described previously in the Usage section, with an additional property for each test object that corresponds to the test result:
+
+```
+{
+  description: 'additionalItems as schema',
+  schema: {
+    items: [{}],
+    additionalItems: { type: "integer" }
+  },
+  tests: [
+    {
+      description: "additional items match schema",
+      data: [ null, 2, 3, 4 ],
+      valid: true,
+      result: {
+        valid: false,
+        errors: [ ... ]
+      }
+    },
+    {
+      description: "additional items do not match schema",
+      data: [ null, 2, 3, "foo" ],
+      valid: false,
+      result: {
+        true
+      }
+    }
+  ]
+}
+```
+
+---
 
 
 JSON Schema Test Suite [![Build Status](https://travis-ci.org/json-schema/JSON-Schema-Test-Suite.png?branch=develop)](https://travis-ci.org/json-schema/JSON-Schema-Test-Suite)
--- test/test.js
+++ test/test.js
@@ -32,7 +32,7 @@ describe('verify test suite loads all json test files', function () {
   }
 
   function compareCount(draft, globPattern, filter, name) {
-    var tests = testsuite.loadSync(filter, draft);
+    var tests = testsuite.loadSync({ filter: filter, draft: draft });
     var files = loadFiles(draft, globPattern);
 
     assert.equal(tests.length, files.length, util.format(errorMessage, name));
@@ -68,92 +68,124 @@ describe('verify test suite loads all json test files', function () {
 
 });
 
-describe('tv4 validator tests', function () {
-  tv4 = require('tv4');
+describe('validator tests', function() {
+  var validatorResults = [];
 
-  var tv4Factory = function (schema, options) {
-    if (typeof schema == 'string') {
-      schema = JSON.parse(text);
-    }
-
-    return {
-      validate: function (json) {
-        try {
-          var valid = tv4.validate(json, schema);
-          return valid ? { valid: true } : { valid: false, errors: [valid] };
-        } catch (err) {
-          return { valid: false, errors: [err.message] };
-        }
-      }
-    }
-  }
-
-  // create a test suite for each draft
-  drafts.forEach(function (draft) {
-    describe(draft, function () {
-
-      var tests = testsuite.testSync(tv4Factory, {}, void 0, draft);
-      tests.forEach(function (test) {
-        describe(test.group, function () {
-          test.schemas.forEach(function (schema) {
-            describe(schema.description, function () {
-              schema.tests.forEach(function (testCase) {
-                it(testCase.description, function () {
-                  var result = testCase.result;
-                  assert.equal(result.valid, testCase.valid);
-                });
-              });
-            });
-          });
-        })
-      });
+  after(function() {
+    validatorResults.forEach(function(validator) {
+      console.log('\n******************************');
+      console.log('validator: %s (%s)', validator.name, validator.draft);
+      console.log('pass: ' + validator.results.pass);
+      console.log('fail: ' + validator.results.fail);
     });
   });
 
-});
+  describe('tv4 validator tests', function () {
+    var tv4 = require('tv4');
 
-describe('z-schema validator tests', function () {
-  var ZSchema = require('z-schema');
-
-  var zschemaFactory = function (schema, options) {
-    var zschema = new ZSchema(options);
-
-    if (typeof schema == 'string') {
-      schema = JSON.parse(text);
-    }
-
-    return {
-      validate: function (json) {
-        try {
-          var valid = zschema.validate(json, schema);
-          return valid ? { valid: true } : { valid: false, errors: zschema.getLastErrors() };
-        } catch (err) {
-          return { valid: false, errors: [err.message] };
-        }
+    var tv4Factory = function (schema, options) {
+      if (typeof schema == 'string') {
+        schema = JSON.parse(text);
       }
-    }
-  }
 
-  // create a test suite for each draft
-  drafts.forEach(function (draft) {
-    describe(draft, function () {
+      return {
+        validate: function (json) {
+          try {
+            var valid = tv4.validate(json, schema);
+            return valid ? { valid: true } : { valid: false, errors: [ tv4.error ] };
+          } catch (err) {
+            return { valid: false, errors: [err.message] };
+          }
+        }
+      };
+    };
 
-      var tests = testsuite.testSync(zschemaFactory, {}, void 0, draft);
-      tests.forEach(function (test) {
-        describe(test.group, function () {
-          test.schemas.forEach(function (schema) {
-            describe(schema.description, function () {
-              schema.tests.forEach(function (testCase) {
-                it(testCase.description, function () {
-                  var result = testCase.result;
-                  assert.equal(result.valid, testCase.valid);
+    // create a test suite for each draft
+    drafts.forEach(function (draft) {
+      var validatorResult = { name: 'tv4', draft: draft, results: { pass: 0, fail: 0 }};
+      validatorResults.push(validatorResult);
+
+      describe(draft, function () {
+
+        var tests = testsuite.testSync(tv4Factory, {}, void 0, draft);
+        tests.forEach(function (test) {
+          describe(test.group, function () {
+            test.schemas.forEach(function (schema) {
+              describe(schema.description, function () {
+                schema.tests.forEach(function (testCase) {
+                  it(testCase.description, function () {
+                    var result = testCase.result;
+                    if (result.valid === testCase.valid) {
+                      validatorResult.results.pass++;
+                    } else {
+                      validatorResult.results.fail++;
+                    }
+
+                    assert.equal(result.valid, testCase.valid);
+                  });
                 });
               });
             });
-          });
-        })
+          })
+        });
       });
     });
+
   });
 
+  describe('z-schema validator tests', function () {
+var ZSchema = require('z-schema');
+
+var zschemaFactory = function (schema, options) {
+  var zschema = new ZSchema(options);
+
+  if (typeof schema == 'string') {
+    schema = JSON.parse(text);
+  }
+
+  return {
+    validate: function (json) {
+      try {
+        var valid = zschema.validate(json, schema);
+        return valid ? { valid: true } : { valid: false, errors: zschema.getLastErrors() };
+      } catch (err) {
+        return { valid: false, errors: [err.message] };
+      }
+    }
+  };
+};
+
+    // create a test suite for each draft
+    drafts.forEach(function (draft) {
+      var validatorResult = { name: 'zschema', draft: draft, results: { pass: 0, fail: 0 }};
+      validatorResults.push(validatorResult);
+
+      describe(draft, function () {
+
+        var tests = testsuite.testSync(zschemaFactory, {}, void 0, draft);
+        tests.forEach(function (test) {
+          describe(test.group, function () {
+            test.schemas.forEach(function (schema) {
+              describe(schema.description, function () {
+                schema.tests.forEach(function (testCase) {
+                  it(testCase.description, function () {
+                    var result = testCase.result;
+                    if (result.valid === testCase.valid) {
+                      validatorResult.results.pass++;
+                    } else {
+                      validatorResult.results.fail++;
+                    }
+
+                    assert.equal(result.valid, testCase.valid);
+                  });
+                });
+              });
+            });
+          })
+        });
+      });
+    });
+
+  });
 });
+
