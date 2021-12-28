583c13381226f8b474e79ffd3643dd31c12b143b
Greg Dennis <gregsdennis@yahoo.com>
2021-12-29 09:00:00 +1300

    process response a bit; output messages

--- report/run.sh
+++ report/run.sh
@@ -2,27 +2,40 @@
 
 for d in ../tests/*/
 do
+    specVersion=$(basename $d)
     for f in $d* # $d contains the ending /
     do
         echo "$f"
         scenarioIndex=0
         jq -cr '.[]' < $f | while read j
         do
-            scenario=$(echo $j | jq '.description')
+            scenario=$(echo $j | jq -r '.description')
             schemaUri="$f#/$scenarioIndex/schema"
             caseIndex=0
             echo $j | jq -c '.tests[]' | while read c
             do
-                case=$(echo $c | jq '.description')
+                case=$(echo $c | jq -r '.description')
                 instanceUri="$f#/$scenarioIndex/tests/$caseIndex/data"
-                valid=$(echo $c | jq '.valid')
+                case "$(echo $c | jq '.valid')" in
+                    true) expected=0 ;;
+                    false) expected=127 ;;  # bash seems to interpret a -1 exit code as 127
+                esac
 
-                ${1} --schema $schemaUri --instance $instanceUri
-                if [[ $? -ne 0 ]]
+                ${1} --schema $schemaUri --instance $instanceUri --spec-version $specVersion
+                evaluated=$?
+
+                if [[ $evaluated -ne $expected ]]
                 then
-                    echo "'$scenario' / '$case' : $?"
+                    echo --schema $schemaUri --instance $instanceUri --spec-version $specVersion / $expected
+                    echo $evaluated
+                    case $evaluated in
+                        127) message="did not validate but should have" ;;
+                        0) message="validated but should not have" ;;
+                        1) message="an error occurred" ;;
+                        2) message="not supported" ;;
+                    esac
+                    echo $message
                 fi
-#               capture result
                 ((caseIndex++))
             done
             ((scenarioIndex++))
