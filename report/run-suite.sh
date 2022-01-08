81ac6a54d1ed7a54a817424e7309b3ace5d20288
Greg Dennis <gregsdennis@yahoo.com>
2022-01-08 20:23:33 +1300

    generate report

--- report/run-suite.sh
+++ report/run-suite.sh
@@ -2,9 +2,13 @@
 
 echo "Running test suite with implementation ${1}"
 
-COMMAND=$(jq -r ".[] | select(.\"docker-image\"==\"${1}\") | .command" /report/implementations.json)
+command=$(jq -r ".[] | select(.\"docker-image\"==\"${1}\") | .command" /report/implementations.json)
+versions=$(jq -r ".[] | select(.\"docker-image\"==\"${1}\") | .versions" /report/implementations.json)
+output="/output/result.yml"
 
-if [[ "${COMMAND}" -eq "" ]]
+sed -e "s/\${IMPLEMENTATION}/${1}/" /report/report-template.yml > ${output}
+
+if [[ "${command}" -eq "" ]]
 then
     echo "Implementation either not found or not configured with a command to execute"
     exit 1
@@ -13,48 +17,51 @@ fi
 for d in ../tests/*/
 do
     specVersion=$(basename $d)
-    echo "$specVersion"
+
+    if ! [[ ${versions[@]} =~ $specVersion ]]
+    then
+        # record that the draft isn't supported
+        continue
+    fi
+
     for f in $d* # $d contains the ending /
     do
         filename=$(basename $f)
-        echo "  ${filename%%.*}"
         scenarioIndex=0
         jq -cr '.[]' < $f | while read j
         do
             scenario=$(echo $j | jq -r '.description')
             schemaUri="$f#/$scenarioIndex/schema"
             caseIndex=0
-            echo "    $scenario"
             echo $j | jq -c '.tests[]' | while read c
             do
                 case=$(echo $c | jq -r '.description')
                 instanceUri="$f#/$scenarioIndex/tests/$caseIndex/data"
                 case "$(echo $c | jq '.valid')" in
-                    true) expected=0 ;;
-                    false) expected=255 ;;
+                    true) expected="valid" ;;
+                    false) expected="invalid" ;;
                 esac
 
-                echo "      $case"
-                ${COMMAND} --schema $schemaUri --instance $instanceUri --spec-version $specVersion
+                ${command} --schema $schemaUri --instance $instanceUri --spec-version $specVersion
                 evaluated=$?
 
-                if [[ $evaluated -ne $expected ]]
-                then
-                    echo --schema $schemaUri --instance $instanceUri --spec-version $specVersion / $expected
-                    echo $evaluated
-                    case $evaluated in
-                        255) message="did not validate but should have" ;;
-                        0) message="validated but should not have" ;;
-                        1) message="an error occurred" ;;
-                        2) message="not supported" ;;
-                    esac
-                    echo $message
-                fi
+                case $evaluated in
+                    255) result="invalid" ;;
+                    0) result="valid" ;;
+                    1) result="error" ;;
+                    2) result="unsupported" ;;
+                esac
+
+                echo "  - draft: ${specVersion}" >> $output
+                echo "    file: ${filename%%.*}" >> $output
+                echo "    scenario: ${scenario}" >> $output
+                echo "    case: ${case}" >> $output
+                echo "    expected: ${expected}" >> $output
+                echo "    result: ${result}" >> $output
+
                 ((caseIndex++))
             done
             ((scenarioIndex++))
         done
-
-        exit 0
     done
 done
