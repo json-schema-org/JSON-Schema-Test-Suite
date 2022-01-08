257384b02f229df2d700fe18360918a6cdf60bbb
Greg Dennis <gregsdennis@yahoo.com>
2022-01-08 16:46:27 +1300

    integrate implementations file; add dockerfile to include jq; update scripts

--- report/run-report.sh
+++ report/run-report.sh
@@ -4,7 +4,13 @@ export IMPLEMENTATION=${1}
 
 echo "Running test suite with '${1}'"
 
+docker image rm test-run -f
+
+docker build -t test-run --build-arg IMPLEMENTATION=${1} .
+
+docker create --name test-run test-run
+
 docker-compose run test-suite
 
-docker-compose logs --no-color > docker-compose.log
-[[ -z "${failed:-}" ]] || exit 1
+# docker-compose logs --no-color > docker-compose.log
+# [[ -z "${failed:-}" ]] || exit 1
--- report/run-suite.sh
+++ report/run-suite.sh
@@ -1,29 +1,41 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "Running test suite with implementation ${1}"
+
+COMMAND=$(jq -r ".[] | select(.\"docker-image\"==\"${1}\") | .command" /report/implementations.json)
+
+if [[ "${COMMAND}" -eq "" ]]
+then
+    echo "Implementation either not found or not configured with a command to execute"
+    exit 1
+fi
 
 for d in ../tests/*/
 do
     specVersion=$(basename $d)
+    echo "$specVersion"
     for f in $d* # $d contains the ending /
     do
-        echo "$f"
+        filename=$(basename $f)
+        echo "  ${filename%%.*}"
         scenarioIndex=0
         jq -cr '.[]' < $f | while read j
         do
             scenario=$(echo $j | jq -r '.description')
             schemaUri="$f#/$scenarioIndex/schema"
             caseIndex=0
+            echo "    $scenario"
             echo $j | jq -c '.tests[]' | while read c
             do
                 case=$(echo $c | jq -r '.description')
                 instanceUri="$f#/$scenarioIndex/tests/$caseIndex/data"
                 case "$(echo $c | jq '.valid')" in
                     true) expected=0 ;;
-                    false) expected=127 ;;  # bash seems to interpret a -1 exit code as 127
+                    false) expected=255 ;;
                 esac
 
-                ${1} --schema $schemaUri --instance $instanceUri --spec-version $specVersion
+                echo "      $case"
+                ${COMMAND} --schema $schemaUri --instance $instanceUri --spec-version $specVersion
                 evaluated=$?
 
                 if [[ $evaluated -ne $expected ]]
@@ -31,7 +43,7 @@ do
                     echo --schema $schemaUri --instance $instanceUri --spec-version $specVersion / $expected
                     echo $evaluated
                     case $evaluated in
-                        127) message="did not validate but should have" ;;
+                        255) message="did not validate but should have" ;;
                         0) message="validated but should not have" ;;
                         1) message="an error occurred" ;;
                         2) message="not supported" ;;
@@ -42,5 +54,7 @@ do
             done
             ((scenarioIndex++))
         done
+
+        exit 0
     done
 done
