2b1ffb7423dacfd08d6de9250ee76f3b3bfb960a
Suprith KG <suprith7kg@gmail.com>
2024-05-01 01:26:38 +0530

    Best practices followed with optimized code

--- bin/annotation_workflow.py
+++ bin/annotation_workflow.py
@@ -1,42 +1,65 @@
 import json
-import os
-import re
 from pathlib import Path
 
-# Function to print GitHub action notice
-def print_github_action_notice(file, url):
-    print(f"::warning file={file},line=1::Annotation: {url}")
+def github_action_notice(file, url, line):
+    """
+    Print GitHub action notice with file path, URL, and line number.
 
-# Read specification URLs from JSON file
-with open("bin/specification_urls.json", "r") as f:
+    Parameters:
+        file (str): File path.
+        url (str): URL.
+        line (int): Line number.
+    """
+    return f"::warning file={file},line={line}::Annotation: {url}"
+
+def find_test_line_number(test_content, test_name):
+    """
+    Find the line number of a test in the JSON content.
+
+    Parameters:
+        test_content (str): JSON content.
+        test_name (str): Test name.
+
+    Returns:
+        int: Line number of the test.
+    """
+    lines = test_content.split("\n")  # Split content into lines
+    for i, line in enumerate(lines, start=1):  # Iterate over lines
+        if test_name in line:  # Check if test name is found in the line
+            return i  # Return the line number if found
+    return 1  # Return None if test name is not found
+
+# Specify the path to the JSON file using pathlib.Path
+json_file_path = Path("bin/specification_urls.json")
+
+# Read specification URLs from JSON file using pathlib.Path
+with json_file_path.open("r", encoding="utf-8") as f:
     urls = json.load(f)
 
 # Iterate through JSON files in tests folder and subdirectories
-for root, dirs, files in os.walk("tests"):
-    for file_name in files:
-        if file_name.endswith(".json"):
-            file_path = os.path.join(root, file_name)
-            # Read the file content
-            with open(file_path, 'r', encoding='utf-8') as f:
-                changed_file_content = f.read()
+for file_path in Path("tests").rglob("*.json"):
+    # Read the file content using pathlib.Path
+    with file_path.open('r', encoding='utf-8') as f:
+        changed_file_content = f.read()
+        
+    # Parse JSON content
+    try:
+        json_content = json.loads(changed_file_content)
+        for test in json_content:
+            if "specification" in test:
+                line_number = find_test_line_number(changed_file_content, test.get("description") )
 
-            # Parse JSON content
-            try:
-                json_content = json.loads(changed_file_content)
-                for test in json_content:
-                    if "specification" in test:
-                        for specification_object in test["specification"]:
-                            for spec, section in specification_object.items():
-                                draft = Path(file_path).parent.name
-                                if spec in ["core", "validation", "hyper-schema"]:
-                                    print_github_action_notice(file_path, urls[draft][spec] + section)
-                                elif spec in ["quote"]:
-                                    continue
-                                elif spec in ["ecma262", "perl5"]:
-                                    print_github_action_notice(file_path, urls[spec] + section)
-                                elif re.match("^rfc\\d+$", spec):
-                                    print_github_action_notice(file_path, urls["rfc"] + spec + ".txt#" + section)
-                                else:
-                                    print_github_action_notice(file_path, urls["iso"])
-            except json.JSONDecodeError:
-                print(f"Failed to parse JSON content for file: {file_path}")
+                for specification_object in test["specification"]:
+                    for spec, section in specification_object.items():
+                        draft = file_path.parent.name
+                        if spec in ["quote"]:
+                            continue
+                        elif spec in ["core", "validation", "hyper-schema"]:
+                            url = urls[draft][spec].format(spec=spec, section=section)
+                        else:
+                            url = urls[spec].format(spec=spec, section=section)
+                        annotation = github_action_notice(file_path, url, line_number)
+                        print(annotation)
+
+    except json.JSONDecodeError:
+        print(f"::error file={file_path}::Failed to parse JSON content")
